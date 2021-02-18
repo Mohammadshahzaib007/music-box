0d7060847ac240c18dead043bd971b04190f44ea
Mohammad Shahzaib <shahzaibmohammad22@gmail.com>
2021-02-18 16:17:57 +0530

    data structure added for the table

--- components/partials/MusicPlayListTable.vue
+++ components/partials/MusicPlayListTable.vue
@@ -12,7 +12,11 @@
       :hide-default-footer="true"
     >
       <template #item.title="{ item }">
-        <PreviewTableItemCard class="mb-8" />
+        <PreviewTableItemCard
+          :title="item.cardData.heading"
+          :description="item.cardData.description"
+          class="mb-8"
+        />
       </template>
 
       <template #item.progress="{item}">
@@ -20,20 +24,20 @@
 
           style="width: 120px; height: 3px; background: #99999F; border-raius: 2px"
         >
-          <div style="width: 80px; height: 3px; background: #2dceef; border-raius: 2px" />
+          <div :style="{width: `${item.songProgressBar}%`, height: '3px', background: '#2dceef', borderRaius: '2px', transition: 'all .3s'}" />
         </div>
       </template>
 
       <template #item.uploadTime="{item}">
         <span
           style="font-weight: normal; font-size: 0.9375rem; letter-spacing: 0.01em; line-height: 32px; color: #99999f;"
-        >22/03/19</span>
+        >{{ item.uploadTime }}</span>
       </template>
 
       <template #item.time="{item}">
         <span
           style="font-weight: normal; font-size: 0.9375rem; letter-spacing: 0.01em; line-height: 32px; color: #99999f;"
-        >49:45</span>
+        >{{ item.time }}</span>
       </template>
     </v-data-table>
   </div>
@@ -41,6 +45,11 @@
 
 <script lang="ts">
 import { Component, Vue } from 'nuxt-property-decorator';
+
+interface CardData {
+   heading: string,
+   description: string
+  }
 
 @Component
 export default class MusicPlayListTable extends Vue {
@@ -68,88 +77,55 @@ headers= [
     class: 'thTitle'
   }]
 
-        itemsList = [
-          {
-            name: 'Frozen Yogurt',
-            calories: 159,
-            fat: 6.0,
-            carbs: 24,
-            protein: 4.0,
-            iron: '1%'
-          },
-          {
-            name: 'Ice cream sandwich',
-            calories: 237,
-            fat: 9.0,
-            carbs: 37,
-            protein: 4.3,
-            iron: '1%'
-          },
-          {
-            name: 'Eclair',
-            calories: 262,
-            fat: 16.0,
-            carbs: 23,
-            protein: 6.0,
-            iron: '7%'
-          },
-          {
-            name: 'Cupcake',
-            calories: 305,
-            fat: 3.7,
-            carbs: 67,
-            protein: 4.3,
-            iron: '8%'
-          },
-          {
-            name: 'Gingerbread',
-            calories: 356,
-            fat: 16.0,
-            carbs: 49,
-            protein: 3.9,
-            iron: '16%'
-          },
-          {
-            name: 'Jelly bean',
-            calories: 375,
-            fat: 0.0,
-            carbs: 94,
-            protein: 0.0,
-            iron: '0%'
-          },
-          {
-            name: 'Lollipop',
-            calories: 392,
-            fat: 0.2,
-            carbs: 98,
-            protein: 0,
-            iron: '2%'
-          },
-          {
-            name: 'Honeycomb',
-            calories: 408,
-            fat: 3.2,
-            carbs: 87,
-            protein: 6.5,
-            iron: '45%'
-          },
-          {
-            name: 'Donut',
-            calories: 452,
-            fat: 25.0,
-            carbs: 51,
-            protein: 4.9,
-            iron: '22%'
-          },
-          {
-            name: 'KitKat',
-            calories: 518,
-            fat: 26.0,
-            carbs: 65,
-            protein: 7,
-            iron: '6%'
-          }
-        ]
+  itemsList: Array<{cardData: CardData, songProgressBar:number, uploadTime: string, time: string}> = [
+    {
+      cardData: {
+        heading: 'Cosmic Queries - Neuroscience',
+        description: 'Psychodelic drugs, dreams, mental health awareness, understanding our reality, and more - Neil deGrasse Tyson, neuroscientist Heather Berlin, PhD, and first-time comic co-host Jackie Hoffman answer fan-submitted questions about neuroscience. NOTE: StarTalk All-Access subscribers...'
+      },
+      songProgressBar: 80,
+      uploadTime: '22/03/19',
+      time: '49:45'
+    },
+    {
+      cardData:
+      {
+        heading: 'Einstein\'s Genius, with Ron Howard',
+        description: 'Neil deGrasse Tyson, filmmaker Ron Howard, astrophysicist Janna Levit, and comic co-host Harrison Greenbaum celebrate and explore the life of Albert Einstein, including his path to scientific enlightenment and the impact of his genius on the world. NOTE: StarTalk All-Access subscrib...'
+      },
+      songProgressBar: 0,
+      uploadTime: '15/03/19',
+      time: '52:19'
+    },
+    {
+      cardData: {
+        heading: '#ICYMI - NASCAR vs Formula 1',
+        description: 'In case you missed this episode on the Playing with Science channel... NASCAR of Formula 1? Which one do you choose? Our host Chuck Nice and Gary O\'Reilly sit down with science communicator Diandra Leslie-Pelecky, PhD, to try and determine who takes the checkered flag.'
+      },
+      songProgressBar: 0,
+      uploadTime: '14/03/19',
+      time: '51:35'
+    },
+    {
+      cardData: {
+        heading: 'Vaccines - Let\'s Make America Smart Again',
+        description: 'Continuing our Let\'s Make America Smart Again series, Neil deGrasse Tyson, comic co-host Felicia Madison, and Pulitzer Prize-winning science journalist Laurie Garret eradicate unfounded disinformation and dish out smart, sensible knowledge and understanding about vaccines...'
+      },
+      songProgressBar: 0,
+      uploadTime: '08/03/19',
+      time: '51:42'
+    },
+    {
+      cardData: {
+        heading: 'Cosmic Queries - Across the Universe',
+        description: 'Continuing our Let\'s Make America Smart Again series, Neil deGrasse Tyson, comic co-host Felicia Madison, and Pulitzer Prize-winning science journalist Laurie Garret eradicate unfounded disinformation and dish out smart, sensible knowledge and understanding about vaccines...'
+      },
+      songProgressBar: 0,
+      uploadTime: '01/03/19',
+      time: '52:15'
+    }
+
+  ]
 };
 </script>
 
--- components/partials/PreviewTableItemCard.vue
+++ components/partials/PreviewTableItemCard.vue
@@ -17,23 +17,26 @@
         class="mb-0"
         style="font-weight: normal; font-size: 0.9375rem; letter-spacing: 0.01em; color: #fff;"
       >
-        Cosmic Queries - Neuroscience
+        {{ title }}
       </p>
       <p
         style=" font-weight: normal; font-size: 0.875rem; letter-spacing: 0.01em;  color: #99999f; line-height: 24px"
       >
-        Psychodelic drugs, dreams, mental health awareness, understanding our reality, and more - Neil deGrasse Tyson, neuroscientist Heather Berlin,
-        PhD, and first-time comic co-host Jackie Hoffman answer fan-submitted questions about neuroscience. NOTE: StarTalk All-Access subscribers...
+        {{ description }}
       </p>
     </div>
   </v-sheet>
 </template>
 
 <script lang="ts">
-import { Component, Vue } from 'nuxt-property-decorator';
+import { Component, Prop, Vue } from 'nuxt-property-decorator';
 
 @Component
 export default class PreviewTableItemCard extends Vue {
+  @Prop()
+  title!: string
 
+  @Prop()
+  description!: string
 };
 </script>
